509eca988cb475bb4366f7576652600088c69369
RaimondsPolis <raimonds.polis.rp@gmail.com>
2024-03-20 14:26:28 +0200

    yuuuuu
    awdawdawdawdawdawdawda

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/teksts")
+def teksts():
+    return render_template("teksts.html")
+
+@app.route("/bilde")
+def bilde():
+    return render_template("bilde.html")
 
 if __name__ =='__main__':
     app.run(port=5000)
